18da124be8702a75df387640a1fed6dfee158b54
masande <gontyelenimasande@gmail.com>
2021-02-23 11:51:06 +0200

    third commit

--- app.py
+++ app.py
@@ -1,62 +1,56 @@
 import sqlite3
 from flask import Flask, render_template, request
 
+
 def init_sqlite_db():
-    conn = sqlite3.connect('mydatabase.db')
+    conn = sqlite3.connect('products.db')
     print("Opened database successfully")
 
-    conn.execute('CREATE TABLE IF NOT EXISTS items (product-name TEXT, brand-name TEXT, available-sizes TEXT, pin TEXT)')
-    print("Table created successfully")
-    conn.execute('CREATE TABLE IF NOT EXISTS orders (productId TEXT, productname TEXT, productphoto TEXT, availablesizes TEXT)')
+    conn.execute('CREATE TABLE items (product_name TEXT, brand_name TEXT, available_sizes TEXT, product_id TEXT, product_image TEXT)')
     print("Table created successfully")
     conn.close()
 
     init_sqlite_db()
 
+
 app = Flask(__name__)
 
-@app.route('/')
-@app.route('/enter-new/')
-def enter_new_student():
-    return render_template('index.html')
+
+#@app.route('/')
+@app.route('/enter-new-item/')
+def enter_new_item():
+    return render_template('product.html')
 
 
-def convertToBinaryData(billie):
-    with open(billie, 'rb') as file:
-        blobData = file.read()
-    return blobData
+@app.route('/add-new-item/', methods=['POST'])
+def add_new_item():
+    if request.method == "POST":
+        try:
+            p_name = request.form['name']
+            b_name = request.form['brand']
+            size = request.form['size']
+            id = request.form['id']
+            image =request.form['image']
 
-#def insertBLOB(productId, productname, productphoto, availablesizes):
-    # try:
-        #sqliteConnection = sqlite3.connect('mydatabase.db')
-        #cursor = sqliteConnection.cursor()
-        #print("Connected to SQLite")
-        #sqlite_insert_blob_query = """ INSERT INTO items
-#                                  (productId, productname, productphoto, availablesizes) VALUES (?, ?, ?, ?)"""
-#        empPhoto = convertToBinaryData(productphoto)
-#        data_tuple = (productId, productname, productphoto, availablesizes)
-#        cursor.execute(sqlite_insert_blob_query, data_tuple)
-#        sqliteConnection.commit()
-#        print("Image and file inserted successfully as a BLOB into a table")
- #       cursor.close()
+            with sqlite3.connect('products.db') as con:
+                cur = con.cursor()
+                cur.execute("INSERT INTO items (product_name, brand_name, available_sizes , product_id, product_image) VALUES (?,?, ?, ?, ?)", (p_name, b_name, size, id, image))
+                con.commit()
+                msg = "Item added successfully."
+        except Exception as e:
+            con.rollback()
+            msg = "Error occurred in insert operation: " + e
 
-#     except sqlite3.Error as error:
-#        print("Failed to insert blob data into sqlite table", error)
-#     finally:
-#        if (sqliteConnection):
-#            sqliteConnection.close()
- #           print("the sqlite connection is closed")
-
-#     insertBLOB(1, "Smith", "E:\pynative\Python\photos\smith.jpg", "E:\pynative\Python\photos\smith_resume.txt")
- #    insertBLOB(2, "David", "E:\pynative\Python\photos\david.jpg", "E:\pynative\Python\photos\david_resume.txt")
+        finally:
+            con.close()
+            return render_template('items.html', msg=msg)
 
 
-
-@app.route('/show-records/', methods=["GET"])
+@app.route('/show-items/', methods=["GET"])
 def show_records():
     records = []
     try:
-        with sqlite3.connect('mydatabase.db') as con:
+        with sqlite3.connect('products.db') as con:
             cur = con.cursor()
             cur.execute("SELECT * FROM items")
             records = cur.fetchall()
@@ -65,6 +59,7 @@ def show_records():
         print("There was an error fetching results from the database.")
     finally:
         con.close()
-        return render_template('index.html', records=records)
+        return render_template('main.html', records=records)
 
-app.run(debug=True)
+
+
