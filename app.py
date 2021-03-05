1bef0a6309249aea02ccb10d8c6776c2c9a8a6ea
masande <gontyelenimasande@gmail.com>
2021-03-05 11:28:12 +0200

    made some changes

--- app.py
+++ app.py
@@ -1,31 +1,44 @@
 import sqlite3
 from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS
+from flask_mail import Mail, Message
 
-global records
-records = []
 
 def init_sqlite_db():
     conn = sqlite3.connect('products.db')
     print("Opened database successfully")
 
-    conn.execute('CREATE TABLE IF NOT EXISTS items (product_name TEXT, brand_name TEXT, available_sizes TEXT, product_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, product_image TEXT)')
+    conn.execute(
+        'CREATE TABLE IF NOT EXISTS items (product_name TEXT, brand_name TEXT, available_sizes TEXT, product_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, product_image TEXT)')
     print("Table(items) created successfully")
-    conn.execute('CREATE TABLE IF NOT EXISTS users(fullname TEXT, email TEXT, username TEXT, password TEXT, address TEXT )')
+    conn.execute(
+        'CREATE TABLE IF NOT EXISTS users(fullname TEXT, email TEXT, username TEXT, password TEXT, address TEXT )')
     print("Table(users) created successfully")
+    conn.execute('CREATE TABLE IF NOT EXISTS logins(fullname TEXT, email TEXT, username TEXT,  comments TEXT)')
+    print("Table(login) created successfully")
     conn.close()
+
 
 def dict_factory(cursor, row):
     d = {}
     for idx, col in enumerate(cursor.description):
         d[col[0]] = row[idx]
     return d
+
+
 init_sqlite_db()
 
 
 app = Flask(__name__)
+mail= Mail(app)
 CORS(app)
 
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'gontyelenimasande@gmail.com'
+app.config['MAIL_PASSWORD'] = 'Sandei99#'
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
 
 @app.route('/')
 @app.route('/enter-new-item/')
@@ -42,11 +55,13 @@ def add_new_item():
             b_name = request.form['brand']
             size = request.form['size']
             id = request.form['id']
-            image =request.form['image']
+            image = request.form['image']
 
             with sqlite3.connect('products.db') as con:
                 cur = con.cursor()
-                cur.execute("INSERT INTO items (product_name, brand_name, available_sizes , product_id, product_image) VALUES (?,?, ?, ?, ?)", (p_name, b_name, size, id, image))
+                cur.execute(
+                    "INSERT INTO items (product_name, brand_name, available_sizes , product_id, product_image) VALUES (?,?, ?, ?, ?)",
+                    (p_name, b_name, size, id, image))
                 con.commit()
                 msg = "Item added successfully."
         except Exception as e:
@@ -87,7 +102,8 @@ def add_new_user():
 
             with sqlite3.connect('products.db') as con:
                 cur = con.cursor()
-                cur.execute("INSERT INTO users (fullname, email, username, password , address) VALUES (?,?, ?, ?, ?)", (p_name, email, username, address,password))
+                cur.execute("INSERT INTO users (fullname, email, username, password , address) VALUES (?,?, ?, ?, ?)",
+                            (p_name, email, username, address, password))
                 con.commit()
                 msg = "Item added successfully."
         except Exception as e:
@@ -100,7 +116,8 @@ def add_new_user():
 
 @app.route('/show-users/', methods=["GET"])
 def show_users():
-
+    global records
+    records = []
     try:
         with sqlite3.connect('products.db') as con:
             con.row_factory = dict_factory
@@ -114,26 +131,58 @@ def show_users():
         con.close()
         return jsonify(records)
 
-
-
-@app.route('/login/', methods=["POST"])
-def login():
-    massage= "You were successfully logged in"
+@app.route('/show-users/', methods=["GET"])
+def pull_users():
+    global recs
+    recs = []
     try:
-        p_name = request.form['name']
-        email = request.form['email']
-        username = request.form['username']
-        address = request.form['address']
-        password = request.form['password']
-
-        for x in records:
-            return jsonify(massage)
+        with sqlite3.connect('products.db') as con:
+            con.row_factory = dict_factory
+            cur = con.cursor()
+            cur.execute("SELECT * FROM users")
+            recs = cur.fetchall()
     except Exception as e:
-        print("There was an error fetching data from table" + str(e))
+        con.rollback()
+        print("There was an error fetching results from the database."+ str(e))
     finally:
-        return jsonify(msg=massage)
+        con.close()
+        return jsonify(recs)
 
 
-if __name__=='__main__':
+@app.route('/contact-us/',methods=['GET','POST'])
+def contact_us():
+    if request.method == "POST":
+        try:
+            p_name = request.form['name']
+            email = request.form['email']
+            username = request.form['username']
+            comment = request.form['comments']
+
+
+            with sqlite3.connect('products.db') as con:
+                cur = con.cursor()
+                cur.execute("INSERT INTO logins (fullname, email, username, comments) VALUES (?,?, ?, ?)",
+                            (p_name, email, username, comment))
+                con.commit()
+                send_email(p_name,email,username,comment)
+                msg = "Item added successfully."
+        except Exception as e:
+            con.rollback()
+            msg = "Error occurred in insert operation: " + str(e)
+
+        finally:
+            con.close()
+            return jsonify(msg)
+
+
+def send_email(p_name,email,username,comment):
+    msg = Message('Hello', sender = 'gontyelenimasande@gmail.com', recipients='{email}')
+    msg.body = f'fullname :{p_name} ' \
+               f'email :{email} ' \
+               f'username :{username}' \
+               f'comment :{comment}'
+    mail.send(msg)
+    return "Sent"
+
+if __name__ == '__main__':
     app.run(debug=True)
-
